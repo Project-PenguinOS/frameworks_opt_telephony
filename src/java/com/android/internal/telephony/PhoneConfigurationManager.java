acac898d2c6f9f9be8586f2be5784d2e0ae6fd06
Akshay Sundaram <quic_akshsund@quicinc.com>
2025-01-16 13:41:57 -0800

    Revert "Broadcast the msim voice capability value"
    This reverts commit c1bac7ee9ce8c8ec6d70d385f971908eea5dc1c9.
    
    Change-Id: I396890a87161d2ff106ddf67fd6b266387f7eba3
    CRs-Fixed: 4027817

--- src/java/com/android/internal/telephony/PhoneConfigurationManager.java
+++ src/java/com/android/internal/telephony/PhoneConfigurationManager.java
@@ -209,7 +209,7 @@ public class PhoneConfigurationManager {
                     " + isDsdsTransitionSupported : " + isDsdsTransitionSupported);
             TelephonyProperties.multi_sim_voice_capability(voiceCapability);
             TelephonyProperties.dsds_transition_supported(isDsdsTransitionSupported);
-            broadcastMsimVoiceCapabilityChanged(voiceCapability);
+            broadcastMsimVoiceCapabilityChanged();
         }
     };
 
@@ -873,10 +873,9 @@ public class PhoneConfigurationManager {
         return mPhones[0].mCi.getModemService();
     }
 
-    private void broadcastMsimVoiceCapabilityChanged(int capability) {
-        log("broadcastMsimVoiceCapabilityChanged with capability: " + capability);
+    private void broadcastMsimVoiceCapabilityChanged() {
+        log("broadcastMsimVoiceCapabilityChanged");
         Intent intent = new Intent(ACTION_MSIM_VOICE_CAPABILITY_CHANGED);
-        intent.putExtra(EXTRAS_MSIM_VOICE_CAPABILITY, capability);
         mContext.sendBroadcast(intent, PERMISSION_MSIM_VOICE_CAPABILITY_CHANGED);
     }
 
