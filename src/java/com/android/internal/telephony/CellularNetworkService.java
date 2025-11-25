153ecb7ac756dae9bd024b2cac7b1825e3625491
youngtaecha <youngtaecha@google.com>
2025-11-25 07:22:08 +0000

    Apply Satellite HAL API for 26Q2
    Update CellularNetworkService to identify non-terrestrial networks using
    the new satelliteTechnology field.
    Implement methods
    - setSatelliteNetworkInfo
    - enablePrioritizedNetworkScan
    - disablePrioritizedNetworkScan
    - networkScanModeChanged
    
    Bug: 461936427
    Test: atest CellularNetworkServiceTest --rerun-until-failure 10 (http://ab/I10300010478063805-passed)
    Flag: com.android.internal.telephony.flags.satellite_26q2_apis
    Change-Id: I37f8adb6c6aadebcc62a1ba36c28e13466f47e07

--- src/java/com/android/internal/telephony/CellularNetworkService.java
+++ src/java/com/android/internal/telephony/CellularNetworkService.java
@@ -455,7 +455,6 @@ public class CellularNetworkService extends NetworkService {
                     RILUtils.convertHalCellIdentity(regResult.cellIdentity);
             final String rplmn = regResult.registeredPlmn;
             final int reasonForDenial = regResult.reasonForDenial;
-            final boolean isNonTerrestrialNetwork = regResult.isNonTerrestrialNetwork;
 
             if (regState == NetworkRegistrationInfo.REGISTRATION_STATE_DENIED
                     && reasonForDenial
@@ -507,6 +506,11 @@ public class CellularNetworkService extends NetworkService {
                             info.getNgranNrVopsInfo().emcSupported,
                             info.getNgranNrVopsInfo().emfSupported);
                     break;
+                case android.hardware.radio.network.AccessTechnologySpecificInfo.nrInfo:
+                    vopsInfo = new NrVopsSupportInfo(info.getNrInfo().nrVopsInfo.vopsSupported,
+                            info.getNrInfo().nrVopsInfo.emcSupported,
+                            info.getNrInfo().nrVopsInfo.emfSupported);
+                    break;
                 case android.hardware.radio.network.AccessTechnologySpecificInfo.geranDtmSupported:
                     cssSupported = info.getGeranDtmSupported();
                     break;
@@ -536,7 +540,6 @@ public class CellularNetworkService extends NetworkService {
                         .setAvailableServices(availableServices)
                         .setCellIdentity(cellIdentity)
                         .setRegisteredPlmn(rplmn)
-                        .setIsNonTerrestrialNetwork(isNonTerrestrialNetwork)
                         .setDataSpecificInfo(
                                 new DataSpecificRegistrationInfo.Builder(MAX_DATA_CALLS)
                                      .setDcNrRestricted(isDcNrRestricted)
