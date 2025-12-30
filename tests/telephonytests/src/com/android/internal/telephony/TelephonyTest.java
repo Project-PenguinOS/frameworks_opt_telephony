2eb49b7622c26d0db6eefaea6acd4daa7475fc5a
Willy Hu <willycwhu@google.com>
2025-12-30 01:55:46 +0000

    [DSRM] Support data stall recovery timer randomization
    - Problem: Network overload due to simultaneous data stall recovery actions
    - Solution: Added `KEY_DATA_STALL_RECOVERY_TIMERS_RANDOMIZATION_MILLIS_LONG_ARRAY`
      carrier config to specify maximum random offsets for each recovery timer stage.
    
    Bug: 478980391
    Test: Local test passed.
          atest DataStallRecoveryManagerTest
    Flag: com.android.internal.telephony.flags.enable_data_stall_recovery_randomization
    Change-Id: Ic1d1ae88537fba2a820d6171cbaa8805ee248513

--- tests/telephonytests/src/com/android/internal/telephony/TelephonyTest.java
+++ tests/telephonytests/src/com/android/internal/telephony/TelephonyTest.java
@@ -575,6 +575,7 @@ public abstract class TelephonyTest {
         lenient().doReturn(true).when(mFeatureFlags).subscriptionPlanEnhancement();
         lenient().doReturn(true).when(mFeatureFlags)
                 .removeTetheringConditionWhenEnablingIndications();
+        lenient().doReturn(true).when(mFeatureFlags).enableDataStallRecoveryRandomization();
 
         WorkerThread.reset();
         TelephonyManager.disableServiceHandleCaching();
