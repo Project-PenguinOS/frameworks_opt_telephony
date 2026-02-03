bcba99863ec6e8f07e3dcdf124a20533dd5e9cc3
sungcheolahn <donaldahn@google.com>
2026-02-03 23:12:01 +0000

    [IMS] Fix NPE in onCarrierRoamingNtnModeChanged
    Add a null check for ServiceState in onCarrierRoamingNtnModeChanged to
    prevent a NullPointerException when tm.getServiceState() returns null.
    This can happen if the service state is not yet available.
    
    Bug: 481128676
    Test: atest ImsPhoneCallTrackerTest
    Flag: EXEMPT BUGFIX
    
    Change-Id: Ic55edd933067aec52198b0ec0817fbd23cd6418d

--- tests/telephonytests/src/com/android/internal/telephony/imsphone/ImsPhoneCallTrackerTest.java
+++ tests/telephonytests/src/com/android/internal/telephony/imsphone/ImsPhoneCallTrackerTest.java
@@ -3009,4 +3009,12 @@ public class ImsPhoneCallTrackerTest extends TelephonyTest {
         mCTUT.getCarrierRoamingNtnListener().onCarrierRoamingNtnModeChanged(true);
         verify(mImsManager, times(1)).setWfcModeInternal(anyInt());
     }
+
+    @Test
+    public void testCarrierRoamingNtnListener_NullServiceState() {
+        doReturn(null).when(mTelephonyManager).getServiceState();
+
+        mCTUT.getCarrierRoamingNtnListener().onCarrierRoamingNtnModeChanged(true);
+        verify(mImsManager, times(1)).setWfcModeInternal(anyInt());
+    }
 }
