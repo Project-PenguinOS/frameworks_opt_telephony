5dfd76ee638f15c1a4fbe2d4587769dd80be8cb1
Thomas Nguyen <tnd@google.com>
2026-03-21 16:53:14 -0700

    Revert "Modify RIL to unbind all mock service in mock modem when setting the"
    This reverts commit b6c408896d508aad48f1adfb247bd88d68adaf1a.
    
    Reason for revert: 494492901
    
    Change-Id: Ie1ab5c135163c23c2bf0dd50c0e35b1aa784c841

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -560,7 +560,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
                 mDisabledRadioServices.get(HAL_SERVICE_RADIO).add(mPhoneId);
             }
 
-            mMockModem.bindAllMockModemServices();
+            mMockModem.bindAllMockModemService();
 
             for (int service = MIN_SERVICE_IDX; service <= MAX_SERVICE_IDX; service++) {
                 if (service == HAL_SERVICE_RADIO) continue;
@@ -603,9 +603,6 @@ public class RIL extends BaseCommands implements CommandsInterface {
             }
 
             if (mMockModem != null) {
-
-                mMockModem.unbindAllMockModemServices();
-
                 mMockModem = null;
                 for (int service = MIN_SERVICE_IDX; service <= MAX_SERVICE_IDX; service++) {
                     if (service == HAL_SERVICE_RADIO) {
